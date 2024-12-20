e1b1fec76e5498ef33d2b0fcc5e644bf4c2c9fd0
Your Name <you@example.com>
2024-12-20 14:41:36 +0800

    Add mics

--- test.sh
+++ test.sh
@@ -4,12 +4,13 @@ RGB=/dev/video2
 RGB_FORMAT=video/x-raw,format=YUY2,width=1280,height=720,framerate=30/1
 EYE=/dev/video4
 EYE_FORMAT=video/x-raw,format=GRAY16_LE,width=1024,height=512,framerate=30/1
-MIC1=alsasrc device="hw:1,0" name=mic1
-MIC1_PIPE=mic1. ! queue ! audioconvert ! voaacenc ! aacparse ! mux.
+MIC1="pulsesrc device=alsa_input.platform-sound.analog-stereo name=mic1 "
+MIC1_PIPE="mic1. ! queue ! audioconvert ! voaacenc ! aacparse ! mux."
 #MIC2=alsasrc device="hw:1,1" name=mic2
 #MIC2_PIPE=mic1. ! queue ! audioconvert ! voaacenc ! aacparse ! mux.
 
 
+echo $MIC1
 export GST_DEBUG=2
 
 gst-launch-1.0 \
